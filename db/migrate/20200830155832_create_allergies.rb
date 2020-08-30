d8224d8438183f894e341254f526cc06457d9be9
jhonny689 <jhonny.chamoun@gmail.com>
2020-08-30 12:10:23 -0400

    created the needed skeleton for the project, models, migration, controllers, routes and views -- all empty still

---
+++ db/migrate/20200830155832_create_allergies.rb
@@ -0,0 +1,11 @@
+class CreateAllergies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :allergies do |t|
+      t.string :name
+      t.integer :ingredient_id
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
